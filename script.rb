c2e218ea8c2ff427dc78ef42d5be815dd3bcb57b
kanu14 <mparkeh14@gmail.com>
2026-08-11 21:01:22 -0700

    testing git connection

---
+++ script.rb
@@ -0,0 +1,12 @@
+# caesar cipher
+# have text and a key to input information and shift
+# split the text into individual items to iterate over
+# convert the characters into ascii/numbers
+# shift and account for rolling from z to a, capitalize as needed
+# put the characters back into an array(?) and return result
+# 
+#
+
+def caesar_cipher(text, key)
+  
+end
