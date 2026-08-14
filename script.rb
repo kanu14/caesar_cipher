4312e95bb7a4a6409a857e3de29c0435c82fe28d
kanu14 <mparkeh14@gmail.com>
2026-08-13 20:38:27 -0700

    converted letters to numbers

--- script.rb
+++ script.rb
@@ -8,5 +8,14 @@
 #
 
 def caesar_cipher(text, key)
+  new_text = Array.new
+  old_text = text.chars
   
+  # old_text.each_with_index { |num| puts old_text.ord}
+  p old_text
+  p old_text[0].ord
+  p new_text
+  old_text.each.map { |letters| puts letters.ord + key}
 end
+
+caesar_cipher("hello world", 5)
