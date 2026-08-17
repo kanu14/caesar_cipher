39a972cff44d5b7c4e33ac942986f10fa7e07e0f
kanu14 <mparkeh14@gmail.com>
2026-08-16 20:26:20 -0700

    working cipher. need edge cases updated

--- script.rb
+++ script.rb
@@ -5,17 +5,28 @@
 # shift and account for rolling from z to a, capitalize as needed
 # put the characters back into an array(?) and return result
 # 
-#
+# if ascii is over 122, make it return to 97
 
 def caesar_cipher(text, key)
   new_text = Array.new
   old_text = text.chars
   
   # old_text.each_with_index { |num| puts old_text.ord}
-  p old_text
-  p old_text[0].ord
-  p new_text
-  old_text.each.map { |letters| puts letters.ord + key}
+  # p old_text
+  # p old_text[0].ord
+  
+  # next line creates an array with the adjusted numbers
+  # new_array = old_text.map { |letters| p  letters.ord + key }
+  new_array = old_text.map do |letters| 
+    if (letters.ord == 32)
+        p letters.chr
+    elsif (letters.ord + key > 122)
+        p (letters.ord + key - 122 + 96).chr
+    else (letters.ord + key).chr
+    end
+  end
+  p new_array
+  
 end
 
 caesar_cipher("hello world", 5)
